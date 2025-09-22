71ae01374c9793ed26ba733183f0d2dee7aaf0a1
Rajesh kumar R <rajeshkumar@mallow-tech.com>
2025-09-22 10:11:27 +0530

    added validation

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
 const validatePassword = (password) => {
+    if (!password) {
+        throw new Error('no password found')
+    }
 
     let count = 0;
 
-    if (password.length <= 8) {
-        count = count+2;
+    if (password.length >= 8) {
+        count = count + 2;
     }
-    if (password.length <= 12) {
-        count = count+2;
+    if (password.length >= 12) {
+        count = count + 2;
     }
-    if(password.match(/D/)) {
-        count ++;
+    if (password.match(/[A-Z]/g)) {
+        count++;
     }
-    if(password.match(/d/)) {
-        count ++;
+    if (password.match(/[a-z]/g)) {
+        count++;
     }
-    if(password.match(/D/)) {
-        count ++;
+    if (password.match(/\d/g)) {
+        count++;
     }
-    if(password.match(/D/)) {}
+    if (password.match(/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~`]/g)) {
+        count++;
+    }
+    if (count <= 3) {
+        return 'Weak';
+    } else if (count <= 6) {
+        return 'Medium';
+    }
+    return 'Strong';
 }
